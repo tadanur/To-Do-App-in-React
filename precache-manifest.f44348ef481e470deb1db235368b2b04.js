8bc7ee1e03cd3d1c218204fd4db4557928d2b3c7
Tugba Adanur <tubabektas@gmail.com>
2020-06-29 12:41:48 -0700

    Updates

---
+++ precache-manifest.f44348ef481e470deb1db235368b2b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068491d30cdd46074f18aea7f8557473",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "044895ee3062be7966e1",
+    "url": "/To-Do-App-in-React/static/css/main.7bbba447.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/To-Do-App-in-React/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-in-React/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044895ee3062be7966e1",
+    "url": "/To-Do-App-in-React/static/js/main.11c64b2c.chunk.js"
+  },
+  {
+    "revision": "8e4ad4c82cdd4e61ac96",
+    "url": "/To-Do-App-in-React/static/js/runtime-main.da4c7d54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
